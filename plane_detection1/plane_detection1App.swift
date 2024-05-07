08f8db5bbf99752b7a4f81ab2d2a1facb1f1e2eb
Krishna Patidar <113329860+krishna-p060@users.noreply.github.com>
2024-05-07 17:41:49 +0530

    code for plane detection

--- plane_detection1/plane_detection1App.swift
+++ plane_detection1/plane_detection1App.swift
@@ -6,16 +6,56 @@
 //
 
 import SwiftUI
+import ARKit
+
 
 @main
 struct plane_detection1App: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//
+//        ImmersiveSpace(id: "ImmersiveSpace") {
+//            ImmersiveView()
+//        }.immersionStyle(selection: .constant(.full), in: .full)
+//    }
+    @State var session = ARKitSession()
+        @State var immersionState: ImmersionStyle = .mixed
+        var body: some Scene {
+            WindowGroup {
+                ContentView()
+            }
+            ImmersiveSpace(id: "appSpace") {
+                //MixedImmersionView()
+                Text("Hello, Mixed Reality!")
+                                .foregroundColor(.white)
+                                .background(Color.black.opacity(0.5))
+                                .frame(width: 200, height: 100)
+                .task {
+                    let planeData = PlaneDetectionProvider(alignments: [.horizontal, .vertical])
+                    
+                    if PlaneDetectionProvider.isSupported {
+                        print("success")
+                        do {
+                            try await session.run([planeData])
+                            for await _ in planeData.anchorUpdates {
+                                // Update app state.
+                                print("plane--")
+                            }
+                        } catch {
+                            print("ARKit session error \(error)")
+                        }
+                    }
+                }
+            }
+            .immersionStyle(selection: $immersionState, in: .mixed)
         }
-
-        ImmersiveSpace(id: "ImmersiveSpace") {
-            ImmersiveView()
-        }.immersionStyle(selection: .constant(.full), in: .full)
-    }
 }
+
+
+
+
+
+
+
